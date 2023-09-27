e4c46e673812e3e82bdcdaf03ee62a78bfa64de6
DESKTOP-AOHKLAB\Haram <spkim8901@gmail.com>
2023-09-26 22:05:11 -0700

    before devide client & server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3001;
+
+// function createData(id, image, name, birthday, gender, job) {
+//     return { id, image, name, birthday, gender, job };
+// }
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.get('/api/customer', (req, res) => {
+//     res.send([
+//         createData(1, "https://placekitten.com/64/64", "fufubao", "200720", "girl", "student"),
+//         createData(2, "https://placekitten.com/64/64", "lebao", "120728", "boy", "manager"),
+//         createData(3, "https://placekitten.com/64/64", "aibao", "130713", "girl", "ceo"),
+//     ]);
+// });
+console.log("hello!!!");
+app.listen(port, () => {
+    console.log(`Listen on port ${port} hehehe`);
+})
